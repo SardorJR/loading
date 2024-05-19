cc05b419e5ce0f6265b035a35762835706de3ce9
Sardor <sardorjn2006@gmail.com>
2024-05-19 19:58:13 +0530

    r

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     let section = document.querySelector('section')
     let body = document.querySelector('.body')
-    section.style.display = 'none';
-    body.style.display = 'block';
+    section.style.display = 'none'
+    body.style.display = 'block'
   }, 7000)
