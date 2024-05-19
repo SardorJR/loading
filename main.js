0b18ccd8c76bd87238ee22444d287554072bede6
Sardor <sardorjn2006@gmail.com>
2024-05-19 16:15:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    let section = document.querySelector('section')
+    let body = document.querySelector('.body')
+    section.style.display = 'none';
+    body.style.display = 'block';
+  }, 6000)
